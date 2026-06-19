ff9b3a4a51f6ae73012b0c21c241d2b6b4ba9b39
Chaerlyn05 <chaerlyn05@github.com>
2026-06-19 10:07:52 +0700

    fix-express5-catchall

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/api/reset', (req, res) => {
 });
 
 // Catch-all: kirim index.html untuk semua route (React SPA)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
